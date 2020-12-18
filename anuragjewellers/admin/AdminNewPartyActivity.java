9873b5810323496fa6516bfe0b0f27a11ca75e03
jay10895 <72381275+jay10895@users.noreply.github.com>
2020-12-18 16:32:28 +0530

    Adding admin\AdminNewPartyActivity

---
+++ anuragjewellers/admin/AdminNewPartyActivity.java
@@ -0,0 +1,16 @@
+package com.example.anuragjewellers.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.anuragjewellery.R;
+
+public class AdminNewPartyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_new_party);
+    }
+}
