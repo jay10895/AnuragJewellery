25563cabf8c2f883db5af5dabe2b6801bacc0bb5
jay10895 <72381275+jay10895@users.noreply.github.com>
2020-12-18 16:33:09 +0530

    Adding admin\AdminSalesmanVepariListActivity

---
+++ anuragjewellers/admin/AdminSalesmanVepariListActivity.java
@@ -0,0 +1,16 @@
+package com.example.anuragjewellers.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.anuragjewellery.R;
+
+public class AdminSalesmanVepariListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_salesman_vepari_list);
+    }
+}
