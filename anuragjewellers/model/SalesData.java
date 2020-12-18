548ab0331756e123bd5e097b3824702ae77af0d7
jay10895 <72381275+jay10895@users.noreply.github.com>
2020-12-18 16:40:20 +0530

    Adding model\SalesData

---
+++ anuragjewellers/model/SalesData.java
@@ -0,0 +1,61 @@
+package com.example.anuragjewellers.model;
+
+public class SalesData {
+    private String sales_id,title,weight,touch,fine,payment,mode;
+
+    public void setSales_id(String sales_id) {
+        this.sales_id = sales_id;
+    }
+
+    public String getSales_id() {
+        return sales_id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setTouch(String touch) {
+        this.touch = touch;
+    }
+
+    public String getTouch() {
+        return touch;
+    }
+
+    public void setFine(String fine) {
+        this.fine = fine;
+    }
+
+    public String getFine() {
+        return fine;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+}
