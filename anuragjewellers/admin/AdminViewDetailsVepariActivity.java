77ac06a8f9589c27f7e6cf464a1e85da13578869
jay10895 <72381275+jay10895@users.noreply.github.com>
2020-12-18 16:33:24 +0530

    Adding admin\AdminViewDetailsVepariActivity

---
+++ anuragjewellers/admin/AdminViewDetailsVepariActivity.java
@@ -0,0 +1,16 @@
+package com.example.anuragjewellers.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.anuragjewellery.R;
+
+public class AdminViewDetailsVepariActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_view_details_vepari);
+    }
+}
